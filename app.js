288b51332521c9884e3a7cb228ed3859284f798b
Mathilde Ferrand Coeurderoy <mathilde.ferrand.coeurderoy@gmail.com>
2018-09-11 13:58:37 +0100

    initial server and file structure

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var cookieParser = require("cookie-parser");
+var cookieSession = require("cookie-session");
+
+//configure the
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cookieSession({
+  secret: "makers-makers-makers"
+}));
 
 app.get("/", function (req, res) {
-  res.send("Hello, World!");
+  var name = req.session.name;
+  res.render("index");
+});
+
+app.post("/register", function (req, res) {
+  req.session.name = req.body.name;
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
