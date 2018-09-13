fe30666b6212a5d040aaebdf5835e7c94c961d22
Andrew Wood <andrew.wood.dev@outlook.com>
2018-09-13 12:46:02 +0100

    Moved connection to separate file

---
+++ server/models/dbconnection.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+// const Sequelize = require('sequelize');
+
+module.exports = (Sequelize) => {
+  if (process.env.npm_lifecycle_event === 'test') {
+    target_db = process.env.ENV_TEST_DATABASE
+  } else {
+    target_db = process.env.ENV_DATABASE
+  };
+  sequelize = new Sequelize(target_db,
+  {
+    host: 'localhost',
+    dialect: 'sqlite3'
+  });
+  return sequelize;
+};
