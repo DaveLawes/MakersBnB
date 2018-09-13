5b1923b99bceb637c27a4793d518e30fa82089f1
Mathilde Ferrand Coeurderoy <mathilde.ferrand.coeurderoy@gmail.com>
2018-09-13 10:25:09 +0100

    ES6 by default and fixed some stuff

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require("cookie-parser");
 const cookieSession = require("cookie-session");
@@ -10,11 +10,10 @@ const path = require('path');
 
 module.exports = app;
 
-//configure the
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use(cookieParser());
@@ -22,12 +21,9 @@ app.use(cookieSession({
   secret: "makers-makers-makers"
 }));
 
-// app.get('*', (req, res) => res.status(200).send(
-//   'Welcome to the beginning of nothingness.',
-// ));
-
 app.get("/", function (req, res) {
   var name = req.session.name;
+
   res.render("pages/index");
 });
 
@@ -41,26 +37,27 @@ app.get("/login", function (req, res) {
   res.render("pages/login");
 });
 
-app.post("/login", function(req, res) {
+app.post("/login", function (req, res) {
   req.session.email = req.body.email;
   res.redirect("/properties");
 });
 
-app.get("/properties", function(req, res) {
+app.get("/properties", function (req, res) {
   var email = req.session.email;
   var name = req.session.name;
-  console.log(email);
-  console.log(name);
-  // don't really want email but without user objects
-  // we are limited to things provided by user on
-  // the log in page. Eventually use FIND on database
+
+/*
+ * Don't really want email but without user objects
+ * We are limited to things provided by user on
+ * The log in page. Eventually use FIND on database
+ */
   res.render("pages/properties", {
-    name: name,
-    email: email
+    email: email,
+    name: name
   });
 });
 
-app.get("/logout", function(req, res) {
+app.get("/logout", function (req, res) {
   req.session = null;
   res.redirect("/");
 });
